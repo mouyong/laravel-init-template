a9439fb47206b672b7225e640eeb0a7f4645a9f4
mouyong <my242551325@gmail.com>
2021-04-19 19:44:10 +0800

    feat: tests getJson

--- stubs/Tests/Traits/RequestTrait.php
+++ stubs/Tests/Traits/RequestTrait.php
@@ -20,4 +20,17 @@ trait RequestTrait
 
         return $this->call('GET', $uri, $query, $cookies, [], $server);
     }
+
+    /**
+     * Visit the given URI with a GET request, expecting a JSON response.
+     *
+     * @param string $uri
+     * @param array $query
+     * @param array $headers
+     * @return \Illuminate\Testing\TestResponse
+     */
+    public function getJson($uri, array $query = [], array $headers = [])
+    {
+        return $this->json('GET', $uri, $query, $headers);
+    }
 }
