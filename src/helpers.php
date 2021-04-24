80da16f9969c1b43876329d8cbb95b8e27650724
mouyong <my242551325@gmail.com>
2021-04-24 10:56:17 +0800

    feat: 添加辅助函数

--- src/helpers.php
+++ src/helpers.php
@@ -1,3 +1,41 @@
 <?php
 
-namespace ZhenMu\LaravelInitTemplate;
+if (! function_exists('url_cache_key')) {
+    /**
+     * Get Current Request Cache Key.
+     *
+     * @param bool $full full url
+     * @return string
+     */
+    function url_cache_key($full = false)
+    {
+        $url = request()->getRequestUri();
+        if ($full) {
+            $url = request()->fullUrl();
+        }
+
+        $queryParams = request()->all();
+
+        ksort($queryParams);
+
+        $queryString = http_build_query($queryParams);
+
+        $fullUrl = "{$url}?{$queryString}";
+
+        $rememberKey = sha1($fullUrl);
+
+        return $rememberKey;
+    }
+}
+
+if (! function_exists('full_url_cache_key')) {
+    /**
+     * Get Current Request Cache Key.
+     *
+     * @return string
+     */
+    function full_url_cache_key()
+    {
+        return url_cache_key(true);
+    }
+}
